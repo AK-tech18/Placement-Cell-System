8d0cab7585826fa53e3c94bf1380f120f7c7775a
Aditya Kaushal <kaushals1@gmail.com>
2026-05-01 22:23:58 +0530

    Connected frontend to live backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend.onrender.com/api",
+  baseURL: "https://placement-cell-system-3.onrender.com/api"
 });
 
 export default API;
